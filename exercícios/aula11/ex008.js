356b711736ec35a85a9a8503f98eb2feb7d28fdf
Pedro Ailton <pedroailton630@gmail.com>
2025-08-20 08:39:21 -0300

    Até o exercício 2 do módulo D

---
+++ exercícios/aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 60.5
+if (vel > 60){ // Condição Simples
+    console.log(`Você ultrapassou o limite de velocidade. A velocidade do seu carro é ${vel}km/h!`)
+}
+
+console.log(`Dirija sempre usando o cinto de segurança`)
